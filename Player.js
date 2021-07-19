3acdf9e33df58c1a77d8a1b2deb205b399c5b172
Nisha <nish.dec@gmail.com>
2021-07-19 22:57:36 +0530

    teacher added images to images folder and changes in sketch.js

--- Player.js
+++ Player.js
@@ -5,6 +5,7 @@ constructor(x,y){
     this.spt=createSprite(this.x,this.y,50,50);
     this.spt.shapeColor="orange";
     this.spt.addAnimation("player",playerAnimation);
+    this.spt.scale=0.05;
 }
 move(xdir,ydir){
     this.spt.x += xdir*grid;
